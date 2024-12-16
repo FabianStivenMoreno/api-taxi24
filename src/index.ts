4554c9dc78e57322dba470b87c8639bd1080bfb8
f.moreno <f.moreno@tcs.com>
2024-12-16 14:11:34 -0500

    fix: se ajusta base del proyecto, dependencias y variables de entorno de ejemplo

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import express from 'express'
 import { conectarDB } from './db/conectarDb'
-// import driverRoutes from './routes/driverRoutes';
-// import tripRoutes from './routes/tripRoutes';
-// import passengerRoutes from './routes/passengerRoutes';
+// import conductorRoutes from './routes/conductorRoutes';
+// import pasajeroRoutes from './routes/pasajeroRoutes';
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -12,17 +11,17 @@ const app = express()
 // Conectar a la base de datos
 conectarDB()
 
-// Middleware para procesar datos JSON
+// Config
 app.use(express.json())
 
 const raiz = process.env.ROOT_PATH || '/'
 // Configurar las rutas
-// app.use('/conductores', driverRoutes);
-// app.use('/viajes', tripRoutes);
-// app.use('/pasajeros', passengerRoutes);
+// app.use(`${raiz}/conductores`, conductorRoutes);
+// app.use(`${raiz}/pasajeros`, pasajeroRoutes);
 
-const port = process.env.PORT || 5000
+const port = process.env.PUERTO || 5000
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`)
+  console.log(`API disponible en: http://localhost:${port}${raiz}`);
 })
